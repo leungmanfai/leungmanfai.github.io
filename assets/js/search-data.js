bee516b8bde77f741beec0fd427dc12e90929c5a
leungmanfai <54782543+leungmanfai@users.noreply.github.com>
2025-01-30 23:12:02 +0000

    Deploying to gh-pages from @ leungmanfai/leungmanfai.github.io@2ea8a4f70cefc14b230d80cc8fb35308506bb556 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -453,13 +453,6 @@ ninja.data = [{
           window.open("https://www.scopus.com/authid/detail.uri?authorId=56404050400", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
